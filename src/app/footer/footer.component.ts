13d6d95728da49a89ef6f0fa452961b13c59bfc5
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2024-02-05 14:28:41 +0100

    Insert icon for Copyright

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCopyright } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faCopyright = faCopyright;
 }
