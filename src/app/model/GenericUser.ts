868a8031227bc9bfd5dbd9352ee9bd04dd5c239a
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2024-03-04 08:52:01 +0100

    Complete form register

---
+++ src/app/model/GenericUser.ts
@@ -0,0 +1,14 @@
+export class GenericUser {
+    firstName!: string;
+    lastName!: string;
+    birthDay!: Date;
+    province!: string;
+    address!: string;
+    email!: string;
+    phone!: string;
+    gender!: string;
+    password!: string;
+    fiscalCode!: string;
+    grant!: number;
+}
+
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
+import { GenericUser } from './../model/GenericUser';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RegisterService } from '../service/register.service';
 
 @Component({
@@ -11,20 +12,45 @@ import { RegisterService } from '../service/register.service';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent implements OnInit {
-
+  @ViewChild('form') form!: NgForm;
   grantValue!: number;
   grantUser: number = 2;
   grantClient: number = 3;
+  private __genericUser: GenericUser = new GenericUser;
+  italianProvinces: string[] = [
+    'Agrigento', 'Alessandria', 'Ancona', 'Aosta', 'Arezzo', 'Ascoli Piceno', 'Asti', 'Avellino',
+    'Bari', 'Barletta-Andria-Trani', 'Belluno', 'Benevento', 'Bergamo', 'Biella', 'Bologna', 'Bolzano',
+    'Brescia', 'Brindisi', 'Cagliari', 'Caltanissetta', 'Campobasso', 'Carbonia-Iglesias', 'Caserta', 'Catania',
+    'Catanzaro', 'Chieti', 'Como', 'Cosenza', 'Cremona', 'Crotone', 'Cuneo', 'Enna', 'Fermo', 'Ferrara', 'Firenze',
+    'Foggia', 'Forlì-Cesena', 'Frosinone', 'Genova', 'Gorizia', 'Grosseto', 'Imperia', 'Isernia', 'La Spezia',
+    'L\'Aquila', 'Latina', 'Lecce', 'Lecco', 'Livorno', 'Lodi', 'Lucca', 'Macerata', 'Mantova', 'Massa-Carrara',
+    'Matera', 'Medio Campidano', 'Messina', 'Milano', 'Modena', 'Monza e della Brianza', 'Napoli', 'Novara', 'Nuoro',
+    'Ogliastra', 'Olbia-Tempio', 'Oristano', 'Padova', 'Palermo', 'Parma', 'Pavia', 'Perugia', 'Pesaro e Urbino',
+    'Pescara', 'Piacenza', 'Pisa', 'Pistoia', 'Pordenone', 'Potenza', 'Prato', 'Ragusa', 'Ravenna', 'Reggio Calabria',
+    'Reggio Emilia', 'Rieti', 'Rimini', 'Roma', 'Rovigo', 'Salerno', 'Sassari', 'Savona', 'Siena', 'Siracusa', 'Sondrio',
+    'Taranto', 'Teramo', 'Terni', 'Torino', 'Trapani', 'Trento', 'Treviso', 'Trieste', 'Udine', 'Varese', 'Venezia',
+    'Verbano-Cusio-Ossola', 'Vercelli', 'Verona', 'Vibo Valentia', 'Vicenza', 'Viterbo'
+  ];
+  selectedProvince: string = '';
 
-  ngOnInit(): void {}
-
-  constructor( private service: RegisterService){
-
+  ngOnInit(): void {
+    this.genericUser.gender = "";
+    this.genericUser.grant = 0;
   }
 
+  constructor( private service: RegisterService){
+    console.log(this.__genericUser);
+  }
+
+  set genericUser(genericiUser: GenericUser){
+    this.__genericUser = genericiUser;
+  }
+  get genericUser(): GenericUser {
+    return this.__genericUser;
+  }
+  
+
   register() {
-    if (this.grantValue){
-      
-    }
+    console.log(this.__genericUser)
     }
 }
